73b207954f41def2389ea0327cbd6098ff8b258f
Unicorn <unicorn@Jaideeps-MacBook-Air-2.local>
2023-04-12 23:54:27 +0530

    saved april 12

---
+++ accounts/api_views.py
@@ -0,0 +1,12 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import CustomUser
+from rest_framework.permissions import AllowAny
+
+
+class TraderAccountCreation(ModelViewSet):
+    permission_classes = [AllowAny]
+    queryset = CustomUser.objects.all()
+    http_method_names = ['post', 'patch', 'put']
+
+    # def get_queryset(self):
+    #
--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from dashboard.views import DashboardViewSet
 from .views import *
 from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
-
+from .api_views import TraderAccountCreation;''[]
 
 urlpatterns = [
     path(r'success/', SuccessView.as_view(), name="success_view"),
@@ -11,5 +11,7 @@ urlpatterns = [
     path(r'login/', LoginView.as_view(), name='account_login'),
     path(r'logout/', LogoutView.as_view(), name='account_logout'),
     path(r'zerodha-auth/', ZerodhaAuthCallback.as_view(), name='zerodha_auth'),
-    path(r'fyers-auth/', FyersAuthCallback.as_view(), name='fyers_auth')
+    path(r'fyers-auth/', FyersAuthCallback.as_view(), name='fyers_auth'),
+    #----------Accounts APIS---------------------------
+    path(r'create-trader-account/', TraderAccountCreation.as_view(), name='create-trader-account')
 ]
