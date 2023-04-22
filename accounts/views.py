8231c0205059a1cd86f32c755e010922e9ccc036
Unicorn <unicorn@Jaideeps-MacBook-Air-2.local>
2023-04-23 00:10:03 +0530

    saved april 22

--- accounts/views.py
+++ accounts/views.py
@@ -43,8 +43,9 @@ class LoginView(FormView):
 
     def form_valid(self, form):
         credentials = form.cleaned_data
+        print(credentials)
         user = authenticate(email=credentials['email'], password=credentials['password'])
-
+        print(user)
         if user is not None:
             login(self.request, user)
             cookie_login = HttpResponseRedirect(reverse_lazy('dashboard:dashboard'))
@@ -150,8 +151,12 @@ class FyersAuthCallback(APIView):
         fyers_respone = fyers.get_profile()
 
         broker_unique_id = fyers_respone.get('data').get('fy_id')
+        # if request.user.is_authenticated:
+        #     broker_conn = IsConnectedBroker.objects.filter(user__id=request.user.id, broker_unique_id=broker_unique_id)
+        # else:
         broker_conn = IsConnectedBroker.objects.filter(broker_unique_id=broker_unique_id)
-        if not broker_conn.exists():
+
+        if (not request.user.is_authenticated) and (not broker_conn.exists()):
             with transaction.atomic():
                 name_list = fyers_respone.get('data').get('name').split(' ')
                 new_user = CustomUser.objects.create(
@@ -174,8 +179,25 @@ class FyersAuthCallback(APIView):
 
             user_auth = authenticate(email=fyers_respone.get('data').get('email_id'), password=fyers_respone.get('data').get('fy_id'))
             login(request, user_auth)
-        else:
-            user_auth = authenticate(email=fyers_respone.get('data').get('email_id'), password=fyers_respone.get('data').get('fy_id'))
-            login(request, user_auth)
+
+        elif request.user.is_authenticated and (not broker_conn.exists()):
+            new_user_broker = IsConnectedBroker.objects.create(
+                broker=OwnBrokers.objects.get(short_title='fyers'),
+                broker_unique_id=broker_unique_id,
+                user=request.user
+            )
+            new_user_broker.save()
+
+        elif request.user.is_authenticated and broker_conn.exists():
+            email = broker_conn.values('user__email').first()
+            if request.user.email == email['user__email']:
+                return redirect_response
+            else:
+                return HttpResponse("Already Broker Connect with some account, please try with new account")
+
+        elif (not request.user.is_authenticated) and (broker_conn.exists()):
+            email = broker_conn.values('user__email').first()
+            user = CustomUser.objects.get(email=email['user__email'])
+            login(request, user, backend='django.contrib.auth.backends.ModelBackend')
 
         return redirect_response
